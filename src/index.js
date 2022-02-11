038a2369f0a42f29bc3ed89e494e4ba8626d3edc
vasantha <vasanthalakshmiballipati@gmail.com>
2022-02-11 13:42:18 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
+// import  neccesary dependendies
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import Bird from './Bird.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './styles.css';
+import Logo from './images/sparrow-png.png';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// component
+function Sparrow(){
+    return(
+        <img src={Logo} alt="" />
+    )
+}
+ 
+//show it to the user
+ReactDom.render(
+    < div className='b'>
+    <div className='box'>
+   <Sparrow/>
+    <Bird/>
+    </div>
+    </div>,
+//to show
+    document.getElementById('root')
+)
